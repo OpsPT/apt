cf1f1a87fbd711e7c9f67b42123cf42c841db33d
Alexandre Martin <alexandre@martin.pm>
2024-03-09 12:19:49 +0100

    fix(ci): fix conditionnal build

--- k9s/opspt.mk
+++ k9s/opspt.mk
@@ -1,4 +1,3 @@
-OPSPT_FOLDER := "$(shell basename $(shell pwd))"
 LATEST_RELEASE := $(shell curl -s https://api.github.com/repos/derailed/k9s/releases/latest | grep "tag_name" | cut -d '"' -f 4) 
 AVAILABLE_VERSION := $(shell tail -n 1 opspt.version) 
 
@@ -6,15 +5,16 @@ check_version:
 	@if [ "$(LATEST_RELEASE)" != "$(AVAILABLE_VERSION)" ]; then \
 		echo "New version available: $(LATEST_RELEASE)"; \
 		sed -i 's/$(strip $(AVAILABLE_VERSION))/$(strip $(LATEST_RELEASE))/g' opspt.version; \
+		sed -i 's/BUILDED=TRUE/BUILDED=FALSE/g' opspt.version; \
 	fi
 
 build_if_updated:
-	@echo "$(shell git log -1 --name-status)"
-	@if git log -1 --name-status | grep $(OPSPT_FOLDER)/opspt.version; then \
+	@if grep "BUILDED=FALSE" opspt.version; then \
 		make -f opspt.mk build; \
 	fi
 
 build:
+	sed -i 's/BUILDED=FALSE/BUILDED=TRUE/g' opspt.version; 
 	mkdir -p dist
 	wget https://github.com/derailed/k9s/releases/download/$(strip $(AVAILABLE_VERSION))/k9s_linux_amd64.deb  -O dist/k9s_buster_amd64_$(strip $(AVAILABLE_VERSION)).deb
 	wget https://github.com/derailed/k9s/releases/download/$(strip $(AVAILABLE_VERSION))/k9s_linux_arm64.deb  -O dist/k9s_buster_arm64_$(strip $(AVAILABLE_VERSION)).deb
--- opspt.example.mk
+++ opspt.example.mk
@@ -1,5 +1,4 @@
 # Example for a Github Repo, adapt as you need to. 
-OPSPT_FOLDER := "$(shell basename $(shell pwd))"
 LATEST_RELEASE := $(shell curl -s https://api.github.com/repos/USER/REPO/releases/latest | grep "tag_name" | cut -d '"' -f 4) 
 AVAILABLE_VERSION := $(shell tail -n 1 opspt.version) 
 
@@ -7,13 +6,15 @@ check_version:
 	@if [ "$(LATEST_RELEASE)" != "$(AVAILABLE_VERSION)" ]; then \
 		echo "New version available: $(LATEST_RELEASE)"; \
 		sed -i 's/$(strip $(AVAILABLE_VERSION))/$(strip $(LATEST_RELEASE))/g' opspt.version; \
+		sed -i 's/BUILDED=TRUE/BUILDED=FALSE/g' opspt.version; \
 	fi
 
 build_if_updated:
-	@if git show --oneline --name-only HEAD | grep $(OPSPT_FOLDER)/opspt.version; then \
+	@if grep "BUILDED=FALSE" opspt.version; then \
 		make -f opspt.mk build; \
 	fi
 
 build:
+	sed -i 's/BUILDED=FALSE/BUILDED=TRUE/g' opspt.version;
 	mkdir -p dist
 	# Add your build commands here
