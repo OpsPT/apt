590ef95fe23069e9e211490e4d081ad261b4e83f
Alexandre Martin <alexandre@martin.pm>
2024-03-23 10:36:26 +0100

    fix(k9s): create ubuntu .deb

--- k9s/opspt.mk
+++ k9s/opspt.mk
@@ -18,7 +18,15 @@ build:
 	mkdir -p dist
 	wget https://github.com/derailed/k9s/releases/download/$(strip $(AVAILABLE_VERSION))/k9s_linux_amd64.deb  -O dist/k9s_buster_amd64_$(strip $(AVAILABLE_VERSION)).deb
 	wget https://github.com/derailed/k9s/releases/download/$(strip $(AVAILABLE_VERSION))/k9s_linux_arm64.deb  -O dist/k9s_buster_arm64_$(strip $(AVAILABLE_VERSION)).deb
+	# Debian
 	cp dist/k9s_buster_amd64_$(strip $(AVAILABLE_VERSION)).deb dist/k9s_bullseye_amd64_$(strip $(AVAILABLE_VERSION)).deb
 	cp dist/k9s_buster_arm64_$(strip $(AVAILABLE_VERSION)).deb dist/k9s_bullseye_arm64_$(strip $(AVAILABLE_VERSION)).deb
 	cp dist/k9s_buster_amd64_$(strip $(AVAILABLE_VERSION)).deb dist/k9s_bookworm_amd64_$(strip $(AVAILABLE_VERSION)).deb
 	cp dist/k9s_buster_arm64_$(strip $(AVAILABLE_VERSION)).deb dist/k9s_bookworm_arm64_$(strip $(AVAILABLE_VERSION)).deb
+	# Ubuntu
+	cp dist/k9s_buster_amd64_$(strip $(AVAILABLE_VERSION)).deb dist/k9s_jammy_amd64_$(strip $(AVAILABLE_VERSION)).deb
+	cp dist/k9s_buster_arm64_$(strip $(AVAILABLE_VERSION)).deb dist/k9s_jammy_arm64_$(strip $(AVAILABLE_VERSION)).deb
+	cp dist/k9s_buster_amd64_$(strip $(AVAILABLE_VERSION)).deb dist/k9s_lunar_amd64_$(strip $(AVAILABLE_VERSION)).deb
+	cp dist/k9s_buster_arm64_$(strip $(AVAILABLE_VERSION)).deb dist/k9s_lunar_arm64_$(strip $(AVAILABLE_VERSION)).deb
+	cp dist/k9s_buster_amd64_$(strip $(AVAILABLE_VERSION)).deb dist/k9s_mantic_amd64_$(strip $(AVAILABLE_VERSION)).deb
+	cp dist/k9s_buster_arm64_$(strip $(AVAILABLE_VERSION)).deb dist/k9s_mantic_arm64_$(strip $(AVAILABLE_VERSION)).deb
